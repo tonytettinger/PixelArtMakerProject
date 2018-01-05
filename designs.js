ab6a60f61b9bbde7e1790b537c318d0c6facb8b0
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-01-05 21:09:34 +0800

    Build2
    Added grid making function with adaptive grid.

--- designs.js
+++ designs.js
@@ -2,9 +2,42 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+//define variables
 
-function makeGrid() {
+var pickedColor=$("#colorPicker").val();
 
-// Your code goes here!
+//event listener for submit button which calls make grid function
+$("#gridSubmit").on("click",function(event){
+    event.preventDefault();
+    makeGrid();
+});
+    
+//to make the grid function. deletes previous grid
+    
+var makeGrid = function() {
+    var rows= $("#input_height").val();
+    var columns= $("#input_width").val();
+    var canvas=$("#pixel_canvas");
+    var rowcol = rows*columns;
+    var power= Math.pow(0.99972, rowcol);
+    var side= (30 * power)+"px";
+    $("td").remove();
+    $("tr").remove();
+    alert(side);
+    for(var i =1; i<=rows; i++){
+        canvas.append("<tr></tr>");
+        $("tr").height(side);
+        for(var j=1; j<=columns; j++){
+            $("#pixel_canvas tr:last").append("<td></td>");
+            $("td").width(side);
+  }
+  }
+    
+    
+};
+    
+/*function paint(){
+
 
 }
+*/
