c21c1739ad8c096af169fd3588ccb9f2d3da8966
Antal Tettinger <antal.tettinger@gmail.com>
2018-01-07 09:31:07 +0800

    Drawing functions updated
    Continuos drawing, color change, erase button implemented need further refining

--- designs.js
+++ designs.js
@@ -3,8 +3,16 @@
 
 // When size is submitted by the user, call makeGrid()
 //define variables
+var mouseDown = false;
 
-var pickedColor=$("#colorPicker").val();
+$(document.body)
+    .on("mousedown", function(){
+    mouseDown=true;
+    })
+    .on("mouseup", function(){
+    mouseDown=false;
+    });
+
 
 //event listener for submit button which calls make grid function
 $("#gridSubmit").on("click",function(event){
@@ -18,24 +26,67 @@ var makeGrid = function() {
     var rows= $("#input_height").val();
     var columns= $("#input_width").val();
     var canvas=$("#pixel_canvas");
+//to scale down from 1 cell = 36px to 1 cell =13px in case of the max of 60x60 number of cells    
     var rowcol = rows*columns;
     var power= Math.pow(0.99972, rowcol);
-    var side= (30 * power)+"px";
+    var side= (36 * power)+"px";
     $("td").remove();
     $("tr").remove();
-    alert(side);
     for(var i =1; i<=rows; i++){
         canvas.append("<tr></tr>");
-        $("tr").height(side);
         for(var j=1; j<=columns; j++){
-            $("#pixel_canvas tr:last").append("<td></td>");
-            $("td").width(side);
+            $("#pixel_canvas tr:last").append("<td class='cell bgwhite'></td>");
+            
   }
+        $("tr").height(side);
+        $("td").width(side);
   }
     
     
 };
-    
+
+var eraser = false;
+$("#delete").on("click", function(){
+    if(eraser){
+        eraser=false;
+    } else{
+        eraser=true;
+    }
+});
+
+$("#pixel_canvas").on("click", "td", function(){
+    var colorPicked=$("#colorPicker").val();
+    if(eraser==false){
+        if($(this).hasClass("bgwhite")){
+            $(this).removeClass("bgwhite");
+            $(this).addClass("hasColor");
+            $(this).css("background-color", colorPicked);
+        }else if($(this).hasClass("hasColor")){
+            $(this).css("background-color", colorPicked);
+        }
+    }else{
+        $(this).addClass("bgwhite");
+        $(this).css("background-color", "white");
+    }
+});
+
+$("#pixel_canvas").on("mouseover", "td", function(){
+    var colorPicked=$("#colorPicker").val();
+    if(mouseDown){
+    if($(this).hasClass("bgwhite")){
+        $(this).removeClass("bgwhite");
+        $(this).addClass("hasColor");
+        $(this).css("background-color", colorPicked);
+    }else if($(this).hasClass("hasColor") && eraser==false){
+        $(this).css("background-color", colorPicked);
+    }else{
+        $(this).addClass("bgwhite");
+        $(this).css("background-color", "white");
+    }
+    }
+});
+
+
 /*function paint(){
 
 
